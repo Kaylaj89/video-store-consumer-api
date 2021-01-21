e06b2810dce923ab2695e42bf8f3f77c41518158
Christabel Sebastian <christabel.escarez@gmail.com>
2021-01-21 14:03:48 -0800

    added route and action fur currently checked out

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,6 +2,7 @@ class CustomersController < ApplicationController
   SORT_FIELDS = %w(name registered_at postal_code)
 
   before_action :parse_query_args
+  before_action :find_customer, only: [:show, :currently_checked_out, :checkout_history]
 
   def index
     if @sort
@@ -28,6 +29,36 @@ class CustomersController < ApplicationController
     render json: customer.as_json(only: [:id, :name, :registered_at, :address, :city, :state, :postal_code, :phone, :account_credit],methods: [:videos_checked_out_count]), status: :ok
   end
 
+    def currently_checked_out
+    message = "#{@customer.name} does not currently have any checked out videos"
+    videos = Video.parameterized_list(params[:sort], params[:n], params[:p]).filter { |video| video.rentals.any? {|rental| rental.customer == @customer && rental.created_at == rental.updated_at} }
+    if videos.empty?
+      render json: {
+        ok: true,
+        message: message,
+        errors: [message]
+      }, status: :ok
+    else
+      render json: videos.as_json(only: [:title, :image_url, :overview, :external_id, :release_date, :inventory], methods: [:available_inventory]), status: :ok
+    end
+    return
+  end
+
+  def checkout_history
+    message = "#{@customer.name} has not previously checked out any videos"
+    videos = Video.parameterized_list(params[:sort], params[:n], params[:p]).filter { |video| video.rentals.any? {|rental| rental.customer == @customer && rental.created_at < rental.updated_at} }
+    if videos.empty?
+      render json: {
+        ok: true,
+        message: message,
+        errors: [message]
+      }, status: :ok
+    else
+      render json: videos.as_json(only: [:title, :checkout_date, :due_date]), status: :ok
+    end
+    return
+  end
+
 private
   def parse_query_args
     errors = {}
@@ -38,6 +69,18 @@ private
 
     unless errors.empty?
       render status: :bad_request, json: { errors: errors }
+    end
+  end
+
+  def customer_params
+    params.permit(:name, :registered_at, :address, :city, :state, :postal_code, :phone, :videos_checked_out_count)
+  end
+
+  def find_customer
+    @customer = Customer.find_by(id: params[:id])
+
+    if @customer.nil?
+      return render json: {ok: false, message: "Customer not found", errors: ['Not Found']}, status: :not_found
     end
   end
 end
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,7 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+  
+  def self.parameterized_list(sort = "id", n = 10, p = 1)
+    return self.all.order(sort).paginate(page: p, per_page: n)
+  end
 end
